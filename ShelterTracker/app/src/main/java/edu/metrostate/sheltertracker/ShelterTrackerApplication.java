c09cb051c590e66257dc90336f755fc5172d25ce
Tim Carlson <timmothy.carlson@gmail.com>
2022-07-16 21:50:33 -0500

    Add example of writing a file to the file system.

--- ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ShelterTrackerApplication.java
+++ ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ShelterTrackerApplication.java
@@ -1,7 +1,11 @@
 package edu.metrostate.sheltertracker;
 
 import android.app.Application;
+import android.util.Log;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +19,30 @@ public class ShelterTrackerApplication extends Application {
         for(int i = 0; i < 20; i++) {
             shelterList.add(new Shelter(Integer.toString(i), "Shelter Number " + i));
         }
+        writeFile();
     }
 
     public List<Shelter> getShelterList() {
         return shelterList;
+    }
+
+
+
+    public void writeFile() {
+
+        // this will put files in the /sdcard/Android/data/edu.metrostate.sheltertracker/files directory
+        File externalDir = getExternalFilesDir(null);
+
+        File outputFile = new File(externalDir, "myfile.txt");
+
+        try {
+            Files.createFile(outputFile.toPath());
+            Files.write(outputFile.toPath(), "My data".getBytes());
+
+        } catch (IOException ex) {
+            Log.e("FileCreation", "Error creating file", ex);
+        }
+
     }
 
 
