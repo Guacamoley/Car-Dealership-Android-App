77dec93a42a72600c75a81bcaa61d6363131180f
Malchar <p_schmitz@email.com>
2022-12-07 15:45:37 -0600

    Revert "Bug fixes for VehicleListActivity"
    This reverts commit 71088e580a2b22756aaf31f6e029dda4f0323c13.

--- CarDealer/app/src/main/java/edu/metrostate/cardealer/VehicleActivity.java
+++ CarDealer/app/src/main/java/edu/metrostate/cardealer/VehicleActivity.java
@@ -74,4 +74,30 @@ public class VehicleActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.vehicle_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
 }
